2603767b1ae98808e6daa3ec921dc70467a7e497
rsteinhart <steinhartrachel@yahoo.ca>
2020-05-27 12:46:26 -0700

    working

--- Scripts/merge_files.sh
+++ Scripts/merge_files.sh
@@ -18,20 +18,14 @@ if [ ! -d "$OUT_DIR/merged_files" ]; then
     mkdir $OUT_DIR/merged_files
 fi
 
-#fhr='00' --> only need this if I'm going to be working with other forecast hour variables.
-
-
 # merge ncep and cmc files seperately
 #     gmerge ens_files/fh000_merged_test grib_files/*.t00z.pgrb2f000*
 
-for fcst in in $(seq -f "%03g" 0 6 384); do
-    # determine if forecast is cmc or ncep files
-    #if [[ "$fcst" == "cmc_"* ]]; then
+for fcst in $(seq -f "%03g" 0 6 384); do
+    # merge cmc files
     gmerge $OUT_DIR/merged_files/cmc_merged.t00z.pgrb2f${fcst} $GRIB_DIR/cmc_*.t00z.pgrb2f000*
-    #fi
 
-    #if [[ "$fcst" == "ncep_"* ]]; then
+    # merge ncep files
     gmerge $OUT_DIR/merged_files/ncep_merged.t00z.pgrb2f${fcst} $GRIB_DIR/ncep_*.t00z.pgrb2f000*
-    #fi
 
 done
