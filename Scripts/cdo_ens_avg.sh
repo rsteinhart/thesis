2dabedd99a70ee49cda67cbe7f7d8b07c1c4d738
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-01 10:36:22 -0700

    ready to test

--- Scripts/cdo_ens_avg.sh
+++ Scripts/cdo_ens_avg.sh
@@ -5,7 +5,7 @@
 # echo 'Starting script to merge ensemble files'
 # echo 'System time is:' `date`
 
-OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/ens_mean
+OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test
 MERGE_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files
 
 if [ ! -d "$OUT_DIR" ]; then
@@ -22,8 +22,8 @@ fi
 
 # cdo ensmean fileis file
 for fcst in $(seq -f "%03g" 0 6 384); do
-    cdo ensmean $MERGE_DIR/cmc_*${fcst} $OUT_DIR/cmc_ens_average.t00z.pgrb2f${fcst}
+    cdo ensmean $MERGE_DIR/cmc_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/cmc_ens_mean.t00z.pgrb2f${fcst}
 
-    cdo ensmean $MERGE_DIR/ncep_*${fcst} $OUT_DIR/ncep_ens_average.t00z.pgrb2f${fcst}
+    cdo ensmean $MERGE_DIR/ncep_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/ncep_ens_mean.t00z.pgrb2f${fcst}
 
 done
