424f4d610700e1b4162434eb15dc6e43f8ddba3b
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-04 10:51:41 -0700

    test ncep and cnc

--- Scripts/grib_var.py
+++ Scripts/grib_var.py
@@ -6,19 +6,37 @@ import pygrib
 
 
 # %%
-file = '/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files/ncep_merged.t00z.pgrb2f384' #for example
-# for file in file_list 
-# make a file list using bash?
+# for ncep files, just use one to get variable names
+ncep_file = '/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files/ncep_merged.t00z.pgrb2f384' #for example
+
 # open the grib file
-grbs = pygrib.open(file)
+grbs = pygrib.open(ncep_file)
 
 # create a new text file ("x" creates the file)
 # switch to "a" to append text file or "w" to overwrite the content of the file
-var_txt = open("var_txt.txt", "x")
+ncep_var_txt = open("ncep_var_txt.txt", "x")
 
 # print the inventory of the file to a text file
 grbs.seek(0)
 for grb in grbs:
     grb = str(grb)
-    var_txt.write(grb)
-    var_txt.write("\n")
+    ncep_var_txt.write(grb)
+    ncep_var_txt.write("\n")
+
+# %%
+# for ncep files, just use one to get variable names
+cmc_file = '/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files/cmc_merged.t00z.pgrb2f384' #for example
+
+# open the grib file
+grbs = pygrib.open(cmc_file)
+
+# create a new text file ("x" creates the file)
+# switch to "a" to append text file or "w" to overwrite the content of the file
+cmc_var_txt = open("cmc_var_txt.txt", "x")
+
+# print the inventory of the file to a text file
+grbs.seek(0)
+for grb in grbs:
+    grb = str(grb)
+    cmc_var_txt.write(grb)
+    cmc_var_txt.write("\n")
