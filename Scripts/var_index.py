4b1c4f6d0dfc532f9bd583f91cd1ffa6e245641d
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-04 14:18:01 -0700

    test

--- Scripts/var_index.py
+++ Scripts/var_index.py
@@ -15,7 +15,19 @@ cmc_var_index = open("cmc_var_index.txt", "x")
 # print the inventory of the file to a text file
 #grbindex.seek(0)
 for grb in grbs:
-    grbindex = pygrib.index(cmc_file,'grb')
+    # grbindex = pygrib.index(cmc_file,'grb')
+    grbindex = pygrib.index(cmc_file,'shortName','typeOfLevel','level')
     # grb = str(grb)
     cmc_var_index.write(grbindex)
     cmc_var_index.write("\n")
+
+# %%
+grbindx = pygrib.index(cmc_file,'shortName','typeOfLevel','level')
+
+
+# selected_grbs=grbindx(shortName='gh',typeOfLevel='isobaricInhPa',level=500)
+# for grb in selected_grbs:
+#     print grb
+# selected_grbs=grbindx(shortName='u',typeOfLevel='isobaricInhPa',level='250')
+# for grb in selected_grbs:
+#     print grb
