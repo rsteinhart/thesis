2228fc49caf5da377b250cc1f9ad6606b0fb6b21
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-04 12:24:32 -0700

    test

--- Scripts/var_index.py
+++ Scripts/var_index.py
@@ -5,7 +5,8 @@ import pygrib
 cmc_file = '/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files/cmc_merged.t00z.pgrb2f384' #for example
 
 # open the grib file
-grbs = pygrib.open(cmc_file)
+# grbs = pygrib.open(cmc_file)
+grbindex = pygrib.index(cmc_file)
 
 # create a new text file ("x" creates the file)
 # switch to "a" to append text file or "w" to overwrite the content of the file
@@ -13,7 +14,7 @@ cmc_var_index = open("cmc_var_index.txt", "x")
 
 # print the inventory of the file to a text file
 grbs.seek(0)
-for grb in grbs:
-    grb = str(grb.index)
+for grb in grbindex:
+    grb = str(grb)
     cmc_var_index.write(grb)
     cmc_var_index.write("\n")
