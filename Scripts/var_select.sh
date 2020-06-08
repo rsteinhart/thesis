331b0d25df7a171aac9aba7c14d9883a7164d98a
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-08 14:54:05 -0700

    updates

--- Scripts/var_select.sh
+++ Scripts/var_select.sh
@@ -14,3 +14,20 @@ vars=(":APCP:"
 var_parse=$(printf "%s|"  "${vars[@]}")
 var_parse="${var_parse%?}"
 wgrib2 <filename> -s | grep -E "($var_parse)" | wgrib2 -i <filename> -grib <outfilename>
+
+#(6) How do I extract the 500 Heights from a file (grib->grib)?
+
+    wgrib -s grib_file | grep ":HGT:500 mb:" | wgrib -i -grib \
+          grib_file -o new_grib_file
+
+
+#(7) How do I eliminate the 500 Heights from a file (grib->grib)?
+
+    wgrib -s grib_file | grep -v ":HGT:500 mb:" | wgrib -i -grib \
+          grib_file -o new_grib_file
+
+
+#(8) How do I extract the winds and temperatures from a file (grib->grib)?
+
+    wgrib -s grib_file | egrep "(:UGRD:|:VGRD:|:TMP:)" | wgrib -i -grib \
+          grib_file -o new_grib_file 
