33dd3893a9d52141bd38c2cb36c7cb1b779acadc
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-10 13:44:55 -0700

    for cmn vars

--- Scripts/cdo_ens_avg.sh
+++ Scripts/cdo_ens_avg.sh
@@ -13,18 +13,43 @@ if [ ! -d "$OUT_DIR" ]; then
     mkdir $OUT_DIR
 fi
 
+if [ ! -d "$OUT_DIR/all_stats" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR/all_stats
+fi
+
 if [ ! -d "$OUT_DIR/ens_mean" ]; then
     echo 'Output directory does not exist yet... creating directory'
     mkdir $OUT_DIR/ens_mean
 fi
 
-# cdo ensmean fileis file
+
+# then need to calc ensemble mean with -ens_processing
 for fcst in $(seq -f "%03g" 0 6 384); do
-    cdo ensmean $MERGE_DIR/cmn_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/cmn_ensmean.t00z.pgrb2f${fcst}
-
-    #cdo ensmean $MERGE_DIR/ncep_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/ncep_ens_mean.t00z.pgrb2f${fcst}
-
+    #echo "getting ensemble details for hsct hour" $fcst
+    #wgrib2  output1 -ens_processing output2 x
+    wgrib2  $MERGE_DIR/cmn_merged.t00z.pgrb2f${fcst} -ens_processing $OUT_DIR/all_stats/stats_ens.t00z.pgrb2f${fcst} 0
+    #echo "merged"
+    #echo "file available at: $OUT_DIR/BC_ens.t00z.pgrb2f${fcst}"
 done
+
+
+# then we extract the ensemble mean from the statistics
+for fcst in $(seq -f "%03g" 0 6 384); do
+    #echo "extracting ensemble mean for hsct hour" $fcst
+    # extract ensemble mean from statistics
+    wgrib2 $OUT_DIR/all_stats/stats_ens.t00z.pgrb2f${fcst} -match  ":ens mean" -grib $OUT_DIR/ens_mean/cmn_ens_mean.t00z.pgrb2f${fcst}.grb
+    #echo "merged"
+    #echo "file available at: $OUT_DIR/BC_ens_mean.t00z.pgrb2f${fcst}.grb"
+done
+
+# # cdo ensmean fileis file
+# for fcst in $(seq -f "%03g" 0 6 384); do
+#     cdo ensmean $MERGE_DIR/cmn_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/cmn_ensmean.t00z.pgrb2f${fcst}
+
+#     #cdo ensmean $MERGE_DIR/ncep_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/ncep_ens_mean.t00z.pgrb2f${fcst}
+
+# done
 
 echo '!!!!!!!!!!  Ensemble averaging complete :) !!!!!!!!!!'
 
