6c5b3d5630d8c52e52bb7957d6278d68b7a9d01d
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-10 13:18:46 -0700

    for cmn vars

--- Scripts/cdo_ens_avg.sh
+++ Scripts/cdo_ens_avg.sh
@@ -5,8 +5,8 @@
 # echo 'Starting script to merge ensemble files'
 # echo 'System time is:' `date`
 
-OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/cmc_vars/merged_files
-MERGE_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/cmc_vars/merged_files
+OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/common_vars
+MERGE_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/common_vars/merged_files
 
 if [ ! -d "$OUT_DIR" ]; then
     echo 'Output directory does not exist yet... creating directory'
@@ -20,7 +20,7 @@ fi
 
 # cdo ensmean fileis file
 for fcst in $(seq -f "%03g" 0 6 384); do
-    cdo ensmean $MERGE_DIR/sp_cmc_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/sp_cmc_ensmean.t00z.pgrb2f${fcst}
+    cdo ensmean $MERGE_DIR/cmc_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/cmn_ensmean.t00z.pgrb2f${fcst}
 
     #cdo ensmean $MERGE_DIR/ncep_merged.t00z.pgrb2f${fcst} $OUT_DIR/ens_mean/ncep_ens_mean.t00z.pgrb2f${fcst}
 
