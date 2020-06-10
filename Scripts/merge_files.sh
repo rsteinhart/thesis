724d3da190d916e415612e5e5d5aace80dad9a6a
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-10 14:13:00 -0700

    final test

--- Scripts/merge_files.sh
+++ Scripts/merge_files.sh
@@ -5,8 +5,8 @@
 # echo 'Starting script to merge ensemble files'
 # echo 'System time is:' `date`
 
-OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/common_vars
-GRIB_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/common_vars/cmc_ncep
+OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/ens_mean
+GRIB_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/common_vars/ens_mean
 
 
 # create directory if it doesn't already exist
@@ -16,9 +16,9 @@ if [ ! -d "$OUT_DIR" ]; then
     mkdir $OUT_DIR
 fi
 
-if [ ! -d "$OUT_DIR/merged_files" ]; then
+if [ ! -d "$OUT_DIR/final" ]; then
     echo 'Output directory does not exist yet... creating directory'
-    mkdir $OUT_DIR/merged_files
+    mkdir $OUT_DIR/final
 fi
 
 # merge ncep and cmc files seperately
@@ -26,7 +26,7 @@ fi
 
 for fcst in $(seq -f "%03g" 0 6 384); do
     # merge cmc files
-    gmerge $OUT_DIR/merged_files/cmn_merged.t00z.pgrb2f${fcst} $GRIB_DIR/*_merged.t00z.pgrb2f${fcst}
+    gmerge $OUT_DIR/final/complete_ens.t00z.pgrb2f${fcst} $GRIB_DIR/*.t00z.pgrb2f${fcst}*
 
     # merge ncep files
     #gmerge $OUT_DIR/merged_files/ncep_merged.t00z.pgrb2f${fcst} $GRIB_DIR/ncep_*.t00z.pgrb2f*
