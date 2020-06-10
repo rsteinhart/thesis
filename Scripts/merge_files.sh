7f0e2046c4d82945c1c302f6b8998f5b1690e735
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-10 11:02:18 -0700

    for cmc sp vars

--- Scripts/merge_files.sh
+++ Scripts/merge_files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -l
 
-source config
+#source config
 
 # echo 'Starting script to merge ensemble files'
 # echo 'System time is:' `date`
+
+OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/cmc_vars
+GRIB_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/cmc_vars
 
 
 # create directory if it doesn't already exist
@@ -23,9 +26,9 @@ fi
 
 for fcst in $(seq -f "%03g" 0 6 384); do
     # merge cmc files
-    gmerge $OUT_DIR/merged_files/cmc_merged.t00z.pgrb2f${fcst} $GRIB_DIR/cmc_*.t00z.pgrb2f000*
+    gmerge $OUT_DIR/merged_files/sp_cmc_merged.t00z.pgrb2f${fcst} $GRIB_DIR/*_cmc_merged.t00z.pgrb2f${fcst}
 
     # merge ncep files
-    gmerge $OUT_DIR/merged_files/ncep_merged.t00z.pgrb2f${fcst} $GRIB_DIR/ncep_*.t00z.pgrb2f000*
+    #gmerge $OUT_DIR/merged_files/ncep_merged.t00z.pgrb2f${fcst} $GRIB_DIR/ncep_*.t00z.pgrb2f*
 
 done
