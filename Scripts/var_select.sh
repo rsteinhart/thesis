d6ff1bf8daf1928634c0e3608d0f0ab79dfc1e6f
rsteinhart <steinhartrachel@yahoo.ca>
2020-06-19 10:56:20 -0700

    go over again

--- Scripts/var_select.sh
+++ Scripts/var_select.sh
@@ -1,45 +1,54 @@
 #!/bin/bash -l
 
-IN_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test/merged_files 
-OUT_DIR=/Users/rsteinhart/DATA/test_data/NAEFS/cdo_test
+IN_DIR=/Volumes/Scratch/Rachel/NAEFS/grib_files 
+OUT_DIR=/Volumes/Scratch/Rachel/NAEFS/ensmean
 
 if [ ! -d "$OUT_DIR/cmc_vars" ]; then
     echo 'Output directory does not exist yet... creating directory'
     mkdir $OUT_DIR/cmc_vars
 fi
 
-if [ ! -d "$OUT_DIR/common_vars" ]; then
+if [ ! -d "$OUT_DIR/cmc_unq" ]; then
     echo 'Output directory does not exist yet... creating directory'
-    mkdir $OUT_DIR/common_vars
+    mkdir $OUT_DIR/cmc_unq
 fi
 
+if [ ! -d "$OUT_DIR/common" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR/common
+fi
+
+
+#wgrib2 $file -match PRES -append -grib ${file}_out.grb
 # grab cmc specific variables
-# for fcst in $(seq -f "%03g" 0 6 384); do
-#       wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(UGRD|VGRD):(5000.0|10000.0|30000.0|40000.0) Pa:" -grib $OUT_DIR/cmc_vars/UV_cmc_merged.t00z.pgrb2f${fcst}
+for fcst in $(seq -f "%03g" 0 6 384); do
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(UGRD|VGRD):(5000.0|10000.0|30000.0|40000.0) Pa:" -grib $OUT_DIR/cmc_unq/cmc_unq.t00z.pgrb2f${fcst}
 
-#       wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(HGT):(5000.0|10000.0|30000.0) Pa:" -grib $OUT_DIR/cmc_vars/HGT_cmc_merged.t00z.pgrb2f${fcst}
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(HGT):(5000.0|10000.0|30000.0) Pa:" -append -grib $OUT_DIR/cmc_unq/cmc_unq.t00z.pgrb2f${fcst}
 
-#       wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(TMP):(5000.0|10000.0) Pa:" -grib $OUT_DIR/cmc_vars/TMP_cmc_merged.t00z.pgrb2f${fcst}
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(TMP):(5000.0|10000.0) Pa:" -append -grib $OUT_DIR/cmc_unq/cmc_unq.t00z.pgrb2f${fcst}
 
-#       wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(RH):(5000.0|10000.0) Pa:" -grib $OUT_DIR/cmc_vars/RH_cmc_merged.t00z.pgrb2f${fcst}
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(RH):(5000.0|10000.0) Pa:" -append -grib $OUT_DIR/cmc_unq/cmc_unq.t00z.pgrb2f${fcst}
 
-#       wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(SNOD):" -grib $OUT_DIR/cmc_vars/HGT_cmc_merged.t00z.pgrb2f${fcst}
-# done
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(SNOD):" -append -grib $OUT_DIR/cmc_unq/cmc_unq.t00z.pgrb2f${fcst}
+done
  
 # grab common cmc ncep variables
 for fcst in $(seq -f "%03g" 0 6 384); do
-      wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(UGRD|VGRD):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/UV_cmc_merged.t00z.pgrb2f${fcst}
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f${fcst}_BC -match ":(UGRD|VGRD|HGT|TMP|RH):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/cmc_common.t00z.pgrb2f${fcst}
 
-      wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(HGT):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/HGT_cmc_merged.t00z.pgrb2f${fcst}
+      wgrib2 $IN_DIR/cmc_*.t00z.pgrb2f$fcst -match ":(WEASD|TCDC|PRES|PRMSL|PWAT|WEL):" -append -grib $OUT_DIR/common_vars/cmc_common.t00z.pgrb2f${fcst}
 
-      wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(TMP):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/TMP_cmc_merged.t00z.pgrb2f${fcst}
+    #   wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(HGT):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/HGT_cmc_merged.t00z.pgrb2f${fcst}
 
-      wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(RH):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/RH_cmc_merged.t00z.pgrb2f${fcst}
+    #   wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(TMP):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/TMP_cmc_merged.t00z.pgrb2f${fcst}
 
-      wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(WEASD|TCDC|PRES|PRMSL|PWAT|WEL):" -grib $OUT_DIR/common_vars/other_cmc_merged.t00z.pgrb2f${fcst}
+    #   wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(RH):(20000.0|25000.0|50000.0|70000.0|85000.0|92500.0|100000.0) Pa:" -grib $OUT_DIR/common_vars/RH_cmc_merged.t00z.pgrb2f${fcst}
+
+      # wgrib2 $IN_DIR/cmc_merged.t00z.pgrb2f$fcst -match ":(WEASD|TCDC|PRES|PRMSL|PWAT|WEL):" -grib $OUT_DIR/common_vars/other_cmc_merged.t00z.pgrb2f${fcst}
 done
 
-echo '!!!!!!!!!!!!! Data has been retreived :) !!!!!!!!!!!!!!!!'
+echo '!!!!!!!!!!!!! Common and unique cmc variables have been selected :) !!!!!!!!!!!!!!!!'
 
 
 
