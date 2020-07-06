07e02fd6dc2eca743665b9dc04746930562eea02
rsteinhart <steinhartrachel@yahoo.ca>
2020-07-06 09:52:37 -0700

    test

---
+++ Scripts/cat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+# Rachel Steinhart
+# July 2020
+
+# cat file1.grb file2.grb ... file[N].grb > concatenated.grb
+
+
+# -------------------------------------------------- #
+data_dir="/Volumes/Scratch/Rachel/NAEFS/ensmean/ens_mean"
+out_dir="/Volumes/Scratch/Rachel/NAEFS/ensmean/ens_mean"
+
+# -------------------------------------------------- #
+
+# create the directory for merged original files
+if [ ! -d "$OUT_DIR" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR
+fi
+
+# -------------------------------------------------- #
+
+for fcst in $(seq -f "%03g" 0 6 384); do
+    cat $data_dir/cdo_cmc_mean_${fcst} $data_dir/cdo_common_mean_${fcst} > $out_dir/FINALmean_${fcst}
+
+
+done
