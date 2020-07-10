2dcb0b2cc05a73ea68fb0878aa578befaa11cfac
rsteinhart <steinhartrachel@yahoo.ca>
2020-07-10 14:37:56 -0700

    working

--- Scripts/cat.sh
+++ Scripts/cat.sh
@@ -3,8 +3,10 @@
 # Rachel Steinhart
 # July 2020
 
-# cat file1.grb file2.grb ... file[N].grb > concatenated.grb
-
+# This script is run after cdo_ens_mean.sh at combines the cmc and common
+# mean files
+# These files should be ready to be input into WRF 
+# BUT need to confirm that the ordering of variabes is ok
 
 # -------------------------------------------------- #
 data_dir="/Volumes/Scratch/Rachel/NAEFS/ensmean/ens_mean"
@@ -21,6 +23,8 @@ fi
 # -------------------------------------------------- #
 
 for fcst in $(seq -f "%03g" 0 6 384); do
+    # cat file1.grb file2.grb ... file[N].grb > concatenated.grb
+    # Doesn't work with file paths :( or at least the output doesn't
     cat cdo_cmc_mean_${fcst} cdo_common_mean_${fcst} > FINALmean_${fcst}.grb
 
 
