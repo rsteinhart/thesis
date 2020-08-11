d4c702be17f67e26e8123b79b7c3f41cef9b267e
rsteinhart <steinhartrachel@yahoo.ca>
2020-08-11 12:01:40 -0700

    ready to run

--- Scripts/var_set.sh
+++ Scripts/var_set.sh
@@ -12,12 +12,12 @@ data_dir="/Volumes/Scratch/Rachel/NAEFS/ensmean"
 for fcst in $(seq -f "%03g" 0 6 384); do
 
     echo $file
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":TMP:" -set_var TT -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":HGT:" -set_var GHT -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":UGRD:" -set_var UU -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":VGRD:" -set_var VV -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":VVEL:" -set_var PRESSURE -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":PRMSL:" -set_var PMSL -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb  
-    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":PRES:" -set_var PSFC -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_0_0:" -set_var var0_2_1_7_0_0 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #TMP
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_3_5:" -set_var var0_2_1_7_3_5 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #HGT
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_2_2:" -set_var var0_2_1_7_2_2 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #UGRD
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_2_3:" -set_var var0_2_1_7_2_3 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #VGRD
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_2_8:" -set_var var0_2_1_7_2_8 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #VVEL
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_3_1:" -set_var var0_2_1_7_3_1 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #PRMSL 
+    wgrib2 $data_dir/FINALmean_${fcst}.grb -if ":var0_4_0_54_3_0:" -set_var var0_2_1_7_3_0 -fi -grib $data_dir/NAEFS.2019082700.${fcst}.grb #PRES
 
 done
