61db35670ca47f94693282676ba911f547524206
rsteinhart <steinhartrachel@yahoo.ca>
2020-11-13 15:35:54 -0800

    select_tmp

---
+++ Scripts/510_scripts/select_tmp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+# Rachel Steinhart
+# November 2020
+
+# -------------------------------------------------- #
+data_dir="/Volumes/Scratch/Rachel/NAEFS/grib_files/2020080100"
+out_dir="/Volumes/Scratch/Rachel/NAEFS/510_temp/det_files"
+
+# -------------------------------------------------- #
+
+# var = TMP = var0_2_1_7_0_0
+
+# grib_get_data -w shortName=t FILE
+
+FILES=$data_dir/*.t00z.pgrb2f000
+
+for file in $FILES; do
+
+    wgrib2 $file -match ":TMP" -grib $out_dir/${file}_TMP.grb
+
+done
