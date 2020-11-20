7432a0d802edc3d87110eadd31e6d36e56bdc2d6
rsteinhart <steinhartrachel@yahoo.ca>
2020-11-20 10:27:43 -0800

    update

--- Scripts/510_scripts/small_grib_BC.sh
+++ Scripts/510_scripts/small_grib_BC.sh
@@ -8,7 +8,7 @@ data_dir="/Volumes/Scratch/Rachel/NAEFS/510_temp/det_files"
 # out_dir="/Volumes/Scratch/Rachel/NAEFS/510_temp/det_files"
 # -------------------------------------------------- #
 
-for file in $data_dir; do
+for file in "$data_dir/"*; do
 
     wgrib2 $file -small_grib -140:-115 49:60 $file_BC.grb
 
