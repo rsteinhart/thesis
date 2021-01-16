4507ae2b830cf5e2d39e477e4c1ad7cc8d6c69b2
rsteinhart <rsteinhart@eoas.ubc.ca>
2021-01-15 16:46:03 -0800

    WORKING

--- scripts/ensmean_scripts/make_ens_v2.sh
+++ scripts/ensmean_scripts/make_ens_v2.sh
@@ -9,22 +9,51 @@ echo 'Starting script to merge ensemble files'
 echo 'System time is:' `date`
 
 # exit when any command fails
-# set -e
+set -e
+
+############################################################
+    # create directory if it doesn't already exist #
+############################################################
+
+echo 'Output directory is' $OUT_DIR
+if [ ! -d "$OUT_DIR" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR
+fi
+
+if [ ! -d "$OUT_DIR/cat_files" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR/cat_files
+fi
+
+if [ ! -d "$OUT_DIR/all_stats" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR/all_stats
+fi
+
+if [ ! -d "$OUT_DIR/ens_mean" ]; then
+    echo 'Output directory does not exist yet... creating directory'
+    mkdir $OUT_DIR/ens_mean
+fi
+
+#######################################
+    # Now start ensmean process #
+#######################################
 
 for fcst in $(seq -f "%03g" 0 6 384); do
     echo "cat-ing for fcst hour" $fcst
     # cat grib_file_1 grib_file_2 ... grib_file_N > output_grib_file
-    cat $GRIB_DIR/*.t00z.pgrb2f${fcst} > $OUT_DIR/merged_files/merged.t00z.pgrb2f${fcst}.grb
-    echo "file available at: $OUT_DIR/merged.t00z.pgrb2f${fcst}"
+    cat $GRIB_DIR/*.t00z.pgrb2f${fcst} > $OUT_DIR/cat_files/cat.t00z.pgrb2f${fcst}.grb
+    echo "file available at: $OUT_DIR/cat_files/cat.t00z.pgrb2f${fcst}"
 done
 
 for fcst in $(seq -f "%03g" 0 6 384); do
     echo "sorting for fcst hour" $fcst
     # 
-     wgrib2 merged.t00z.pgrb2f${fcst}.grb | \
+     wgrib2 $OUT_DIR/cat_files/cat.t00z.pgrb2f${fcst}.grb | \
        sort -t: -k5,5 -k4,4 |  \
-       wgrib2 -i merged.t00z.pgrb2f${fcst}.grb -grib sort_merged.t00z.pgrb2f${fcst}.grb
-    echo "file available at: $OUT_DIR/merged.t00z.pgrb2f${fcst}"
+       wgrib2 -i /$OUT_DIR/cat_files/cat.t00z.pgrb2f${fcst}.grb -grib /$OUT_DIR/cat_files/sort_cat.t00z.pgrb2f${fcst}.grb
+    echo "file available at: $OUT_DIR/cat_files/sort_cat.t00z.pgrb2f${fcst}"
 done
 
 
@@ -32,9 +61,9 @@ done
 for fcst in $(seq -f "%03g" 0 6 384); do
     echo "getting ensemble details for hsct hour" $fcst
     #wgrib2  output1 -ens_processing output2 x
-    wgrib2  $OUT_DIR/merged_files/sort_merged.t00z.pgrb2f${fcst}.grb -ens_processing $OUT_DIR/all_stats/ens.t00z.pgrb2f${fcst} 0
+    wgrib2  $OUT_DIR/cat_files/sort_cat.t00z.pgrb2f${fcst}.grb -ens_processing $OUT_DIR/all_stats/ens.t00z.pgrb2f${fcst} 0
     echo "ens_processing"
-    echo "file available at: $OUT_DIR/BC_ens.t00z.pgrb2f${fcst}"
+    echo "file available at: $OUT_DIR/all_stats/ens.t00z.pgrb2f${fcst}"
 done
 
 
@@ -44,21 +73,21 @@ for fcst in $(seq -f "%03g" 0 6 384); do
     # extract ensemble mean from statistics
     wgrib2 $OUT_DIR/all_stats/ens.t00z.pgrb2f${fcst} -match  ":ens mean" -grib $OUT_DIR/ens_mean/ens_mean.t00z.pgrb2f${fcst}.grb
     echo "mean extracted"
-    echo "file available at: $OUT_DIR/BC_ens_mean.t00z.pgrb2f${fcst}.grb"
+    echo "file available at: $OUT_DIR/ens_mean/ens_mean.t00z.pgrb2f${fcst}.grb"
 done
 
 
 ### for sorting files after <cat>
-"""
-     wgrib2 IN.grb | \
-       sed -e 's/:UGRD:/:UGRDa:/' -e 's/:VGRD:/:UGRDb:/' | \
-       sort -t: -k3,3 -k6n,6 -k5,5 -k4,4 |  \
-       wgrib2 -i IN.grb -grib OUT.grb
+# """
+#      wgrib2 IN.grb | \
+#        sed -e 's/:UGRD:/:UGRDa:/' -e 's/:VGRD:/:UGRDb:/' | \
+#        sort -t: -k3,3 -k6n,6 -k5,5 -k4,4 |  \
+#        wgrib2 -i IN.grb -grib OUT.grb
 
 
-     line 1: makes standard inventory
-     line 2: converts UGRD -> UGRDa and VGRD -> UGRDb 
-      so VGRD should follow UGRD when sorted -  NOT USED
-     line 3: sort by date code, forecast time, level and variable
-     line 4: wgrib2 reads inventory, writes out grib file as sorted by inventory
-"""
+#      line 1: makes standard inventory
+#      line 2: converts UGRD -> UGRDa and VGRD -> UGRDb 
+#       so VGRD should follow UGRD when sorted -  NOT USED
+#      line 3: sort by date code, forecast time, level and variable
+#      line 4: wgrib2 reads inventory, writes out grib file as sorted by inventory
+# """
